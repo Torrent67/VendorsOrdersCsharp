70a2578a9ddedc8ca2338bbb89f4361c74901ebf
Marqis Ramos <atair01@yahoo.com>
2019-08-02 20:28:42 -0700

    having some issues with dotnet watch but working on it, saying its already in use IUNNO

--- Bakery/Controllers/ItemVendorController.cs
+++ Bakery/Controllers/ItemVendorController.cs
@@ -1,30 +1,57 @@
+using System.Collections.Generic;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Bakery.Models;
-using System.Collections.Generic;
 
 namespace Bakery.Controllers
 {
-  public class ItemsController : Controller
+   public class VendorsController : Controller
   {
 
-    [HttpGet("/items")]
+    [HttpGet("/vendors")]
     public ActionResult Index()
     {
-      List<Vendors> allItems = Vendors.GetAll();
-      return View(allItems);
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
     }
 
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm(string vendorName)
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
     {
       return View();
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string vendorName)
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDesc)
     {
-      Vendors myItem = new Vendors(vendorName);
+      Vendor newVendor = new Vendor(vendorName,vendorDesc);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrder = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrder);
+      return View(model);
+    }
+
+
+    // This one creates new Items within a given Vendor, not new Categories:
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string NewOrder, string orderDesc, int price, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(NewOrder, orderDesc, price, orderDate);
+      foundVendor.AddItem(newOrder);
+      List<Order> vendorOrder = foundVendor.Orders;
+      model.Add("orders", vendorOrder);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
     }
 
   }
---
+++ Bakery/Models/ItemOrders.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Models
+{
+    public class Order
+    {
+        public string newOrder { get; set; }
+        public string orderDesc { get; set; }
+        public int price { get; set; }
+        public string orderDate { get; set; }
+        public int Id { get; }
+        private static List<Order> _instances = new List<Order> { };
+
+        public Order(string NewOrder, string Description, int Price, string date)
+        {
+            newOrder = NewOrder;
+            orderDesc = Description;
+            price = Price;
+            orderDate = date;
+            _instances.Add(this);
+            Id = _instances.Count;
+        }
+
+        public static List<Order> GetAll()
+        {
+            return _instances;
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+
+        public static Order Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
+    }
+}
